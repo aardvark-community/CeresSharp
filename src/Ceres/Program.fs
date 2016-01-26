892d3c4a4dba8bc817cc9c441a21c7c09f3c4a45
Robert F. Tobler <rft@vrvis.at>
2016-01-26 15:12:06 +0100

    [ceres] camera calibration API

--- src/Ceres/Program.fs
+++ src/Ceres/Program.fs
@@ -1,9 +1,9 @@
 ﻿namespace Ceres
+open System
+open System.Runtime.InteropServices
+#nowarn "9"
 
 module Test =
-
-    open System
-    open System.Runtime.InteropServices
 
     [<Literal>]
     let lib = "CeresCPP.dll"
@@ -11,6 +11,28 @@ module Test =
     [<DllImport(lib)>]
     extern int main()
 
+[<StructLayout(LayoutKind.Sequential)>]
+type Observation =
+    struct
+        val mutable public ObsX : float
+        val mutable public ObsY : float
+        val mutable public RefX : float
+        val mutable public RefY : float
+
+        new(ox,oy,rx,ry) = { ObsX = ox; ObsY = oy; RefX= rx; RefY = ry }
+    end
+
+module CameraCalibration =
+    [<Literal>]
+    let lib = "CeresCPP.dll"
+
+    [<DllImport(lib)>]
+    extern int private calibrate_camera(int observation_count, Observation[] observations, double[] camera)
+        
+    let CalibrateCamera 
+        (observations: Observation[])
+        (camera: double[]) : int =
+        calibrate_camera(observations.Length, observations, camera)
 
 //[<EntryPoint>]
 //let main argv = 
