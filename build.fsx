6fae4dc0e9d26c0222c4190b64f0ab6de7566417
Harald Steinlechner <hs@vrvis.at>
2016-02-02 16:48:30 +0100

    updated build script

--- build.fsx
+++ build.fsx
@@ -1,21 +1,27 @@
-#I @"packages/FAKE/tools/"
-#I @"packages/Paket.Core/lib/net45"
-#I @"bin"
-#r @"System.Xml.Linq"
-#r @"FakeLib.dll"
-#r @"Paket.Core.dll"
-#r @"Aardvark.Fake.dll"
-
+#load @"paket-files/build/vrvis/Aardvark.Fake/DefaultSetup.fsx"
 
 open Fake
 open System
 open System.IO
 open System.Diagnostics
 open Aardvark.Fake
+open Fake.Testing
+
 
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-DefaultTargets.install ["src/Ceres.sln"]
+let sln = ["src/Ceres.sln"]
+DefaultSetup.install sln
 
-// start build
-RunTargetOrDefault "Default"
+Target "Deploy" (fun () ->
+    Fake.MSBuildHelper.MSBuild "bin/Release" "build" [ "Configuration", "Deploy" ] sln |> printfn "%A"
+)
+
+"Deploy" ==> "CreatePackage"
+
+#if DEBUG
+do System.Diagnostics.Debugger.Launch() |> ignore
+#endif
+
+
+entry()
