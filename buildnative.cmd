d47fcefcab0a7b3ec627df8b7a0a29344dc48704
Georg Haaser <krauthaufen@awx.at>
2022-02-02 23:08:36 +0100

    x64-windows

--- buildnative.cmd
+++ buildnative.cmd
@@ -1,10 +1,9 @@
 @echo off
 
-mkdir .vcpkg
 git clone https://github.com/Microsoft/vcpkg.git ./.vcpkg/vcpkg --depth 1
 cmd /C ".vcpkg\vcpkg\bootstrap-vcpkg.bat -disableMetrics"
-.vcpkg\vcpkg\vcpkg.exe install ceres --triplet x64-windows-static
+.vcpkg\vcpkg\vcpkg.exe install ceres --triplet x64-windows
 
-cmake -S src\CeresNative -B src\CeresNative\build -DCMAKE_TOOLCHAIN_FILE="%~dp0\.vcpkg\vcpkg\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Release
+cmake -S src\CeresNative -B src\CeresNative\build -DCMAKE_TOOLCHAIN_FILE="%~dp0\.vcpkg\vcpkg\scripts\buildsystems\vcpkg.cmake"  -DCMAKE_BUILD_TYPE=Release
 
 cmake --build src\CeresNative/build --config Release --target install
