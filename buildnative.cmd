0fea9331df4f21be7da4908e29683dbae6b215be
Georg Haaser <krauthaufen@awx.at>
2022-02-03 00:32:46 +0100

    windows build only-release

--- buildnative.cmd
+++ buildnative.cmd
@@ -1,9 +1,15 @@
 @echo off
 
+cmd /C "rmdir /S /Q .vcpkg"
+
 git clone https://github.com/Microsoft/vcpkg.git ./.vcpkg/vcpkg --depth 1
 cmd /C ".vcpkg\vcpkg\bootstrap-vcpkg.bat -disableMetrics"
-.vcpkg\vcpkg\vcpkg.exe install ceres --triplet x64-windows-static-md
 
-cmake -S src\CeresNative -B src\CeresNative\build -DCMAKE_TOOLCHAIN_FILE="%~dp0\.vcpkg\vcpkg\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static-md -DCMAKE_BUILD_TYPE=Release
+.vcpkg\vcpkg\triplets\community\x64-windows-static-md.cmake .vcpkg\vcpkg\triplets\community\x64-windows-static-md-rel.cmake
+echo set(VCPKG_BUILD_TYPE release) >> .vcpkg\vcpkg\triplets\community\x64-windows-static-md-rel.cmake
+
+.vcpkg\vcpkg\vcpkg.exe install ceres --triplet x64-windows-static-md-rel
+
+cmake -S src\CeresNative -B src\CeresNative\build -DCMAKE_TOOLCHAIN_FILE="%~dp0\.vcpkg\vcpkg\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static-md-rel -DCMAKE_BUILD_TYPE=Release
 
 cmake --build src\CeresNative/build --config Release --target install
