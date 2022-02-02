e4b4e3a298578c4e5294c3fbdd63dc3773b28b8c
Georg Haaser <krauthaufen@awx.at>
2022-02-02 22:17:12 +0100

    windows CI test 5

--- buildnative.cmd
+++ buildnative.cmd
@@ -3,7 +3,7 @@
 mkdir .vcpkg
 git clone https://github.com/Microsoft/vcpkg.git ./.vcpkg/vcpkg --depth 1
 .vcpkg\vcpkg\bootstrap-vcpkg.bat
-.vcpkg\vcpkg\vcpkg install ceres --triplet x64-windows-release
+.vcpkg\vcpkg\vcpkg.exe install ceres --triplet x64-windows-release
 
 cmake -S src\CeresNative/ -B src\CeresNative\build \
     -DCMAKE_TOOLCHAIN_FILE="%~dp0\.vcpkg\vcpkg\scripts\buildsystems\vcpkg.cmake" \
